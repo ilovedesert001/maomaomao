c002f801b4fb74d5fdbfdda2e26786936c4523c3
maomaomao <ilovedesert001@outlook.com>
2019-01-15 18:46:22 +0800

    1.add tfa

---
+++ webapps/tfa/precache-manifest.831a85ddc785cc5722af8187edfc2002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "af64822b671d507894af",
+    "url": "./static/js/main.af64822b.chunk.js"
+  },
+  {
+    "revision": "a1b22ff7c51cf5b80f82",
+    "url": "./static/js/1.a1b22ff7.chunk.js"
+  },
+  {
+    "revision": "af64822b671d507894af",
+    "url": "./static/css/main.dce42278.chunk.css"
+  },
+  {
+    "revision": "a1b22ff7c51cf5b80f82",
+    "url": "./static/css/1.12daf138.chunk.css"
+  },
+  {
+    "revision": "8a635ed9b027fc4ed04e030930b6832d",
+    "url": "./index.html"
+  }
+];
